115665aeb6ac69f37d912bea2dc107be410f28dd
Kevin <kevinrisqi22@gmail.com>
2019-11-26 11:16:06 +0700

    Add detail barang in form penjualan

--- DetailBarang.vb
+++ DetailBarang.vb
@@ -2,7 +2,12 @@
 Public Class DetailBarang
 
     Sub tampilBarang()
-        
+        Call koneksi()
+        Da = New OdbcDataAdapter("SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori", Conn)
+        Ds = New DataSet
+        Da.Fill(Ds, "barang")
+        BunifuCustomDataGrid1.DataSource = Ds.Tables("barang")
+        BunifuCustomDataGrid1.ReadOnly = True
     End Sub
 
     Private Sub DetailBarang_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
@@ -16,7 +21,35 @@ Public Class DetailBarang
     End Sub
 
     Private Sub search_OnTextChange(sender As Object, e As EventArgs) Handles search.OnTextChange
-        
+        If RadioButton1.Checked Then
+            Call koneksi()
+            Dim searchData As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE barang.id_barang  like '%" & search.text & "%'"
+            Cmd = New OdbcCommand(searchData, Conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+            If Rd.HasRows Then
+                Call koneksi()
+                Dim query As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE barang.id_barang like '%" & search.text & "%'"
+                Da = New OdbcDataAdapter(query, Conn)
+                Ds = New DataSet
+                Da.Fill(Ds, "barang")
+                BunifuCustomDataGrid1.DataSource = Ds.Tables("barang")
+            End If
+        Else
+            Call koneksi()
+            Dim searchData As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE barang.nama_barang  like '%" & search.text & "%'"
+            Cmd = New OdbcCommand(searchData, Conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+            If Rd.HasRows Then
+                Call koneksi()
+                Dim query As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE barang.nama_barang like '%" & search.text & "%'"
+                Da = New OdbcDataAdapter(query, Conn)
+                Ds = New DataSet
+                Da.Fill(Ds, "barang")
+                BunifuCustomDataGrid1.DataSource = Ds.Tables("barang")
+            End If
+        End If
     End Sub
 
     Private Sub BunifuCustomDataGrid1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles BunifuCustomDataGrid1.CellClick
@@ -24,11 +57,18 @@ Public Class DetailBarang
     End Sub
 
     Private Sub BunifuCustomDataGrid1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles BunifuCustomDataGrid1.CellContentClick
-        
+
     End Sub
 
     Private Sub BunifuCustomDataGrid1_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles BunifuCustomDataGrid1.CellDoubleClick
-        
+        Dim i As Integer
+        i = BunifuCustomDataGrid1.CurrentRow.Index
+
+        FormPenjualan.kodeBarang.Text = BunifuCustomDataGrid1.Item(1, i).Value
+        FormPenjualan.namaBarang.Text = BunifuCustomDataGrid1.Item(2, i).Value
+        FormPenjualan.hargaSatuan.Text = BunifuCustomDataGrid1.Item(4, i).Value
+        Me.Dispose()
+        FormPenjualan.kodeBarang.Focus()
     End Sub
 
 End Class
