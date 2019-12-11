486c41041526b8e82ba41434f0bf89dc8eb51fd0
firismanda <ryuka.1111@gmail.com>
2019-12-11 09:42:50 +0700

    Add function totalProduct

--- FormDashboard.vb
+++ FormDashboard.vb
@@ -9,11 +9,17 @@ Public Class FormDashboard
     End Sub
 
     Sub totalUser()
-        
+        Call koneksi()
+        Cmd = New OdbcCommand("SELECT COUNT(id_admin) FROM admin", Conn)
+        Dim count As Integer = Cmd.ExecuteScalar
+        lblUser.Text = count
     End Sub
 
     Sub totalProduct()
-        
+        Call koneksi()
+        Cmd = New OdbcCommand("SELECT COUNT(*) FROM barang", Conn)
+        Dim count As Integer = Cmd.ExecuteScalar
+        lblProduct.Text = count
     End Sub
 
     Sub totalSales()
