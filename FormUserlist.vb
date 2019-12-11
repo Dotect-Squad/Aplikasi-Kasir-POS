0748e3697afbedd305dadfb867721a1bd7af580a
Padang Basudewa <padangbasudewa4@gmail.com>
2019-12-11 10:05:31 +0700

    pull from master

--- FormUserlist.vb
+++ FormUserlist.vb
@@ -134,4 +134,7 @@ Public Class FormUserlist
         End If
     End Sub
 
+    Private Sub BunifuCustomDataGrid1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles BunifuCustomDataGrid1.CellContentClick
+
+    End Sub
 End Class
